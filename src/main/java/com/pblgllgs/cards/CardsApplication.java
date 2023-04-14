782ce03a1d8f1e677e5d8fffeb96f9b942bfc85e
pbl.gllgs <pbl.gllgs@gmail.com>
2023-04-14 13:00:50 -0400

    config server

--- src/main/java/com/pblgllgs/cards/CardsApplication.java
+++ src/main/java/com/pblgllgs/cards/CardsApplication.java
@@ -3,11 +3,13 @@ package com.pblgllgs.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("com.pblgllgs.cards.controller")})
 @EnableJpaRepositories("com.pblgllgs.cards.repository")
 @EntityScan("com.pblgllgs.cards.model")
--- src/main/java/com/pblgllgs/cards/controller/CardsController.java
+++ src/main/java/com/pblgllgs/cards/controller/CardsController.java
@@ -1,9 +1,15 @@
 package com.pblgllgs.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.pblgllgs.cards.config.CardsServiceConfig;
 import com.pblgllgs.cards.model.Cards;
 import com.pblgllgs.cards.model.Customer;
+import com.pblgllgs.cards.model.Properties;
 import com.pblgllgs.cards.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +21,9 @@ public class CardsController {
     @Autowired
     private CardsRepository cardsRepository;
 
+    @Autowired
+    CardsServiceConfig cardsConfig;
+
     @PostMapping("/myCards")
     public List<Cards> getAccountDetails(@RequestBody Customer customer){
         List<Cards> cards =  cardsRepository.findByCustomerId(customer.getCustomerId());
@@ -23,5 +32,14 @@ public class CardsController {
         }else{
             return null;
         }
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
